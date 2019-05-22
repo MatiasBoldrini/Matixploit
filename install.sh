831ea08c69d87a707e2702204c579b4ee3c732d7
ShadowFighter99 <50892896+ShadowFighter99@users.noreply.github.com>
2019-05-22 09:47:55 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "copying files to /usr/local/bin.."
-sudo cp matisploit mts helpp /usr/local/bin
 sudo chmod +x mts matisploit helpp
+sudo cp matisploit mts helpp /usr/local/bin
 echo "Instalation succesful , type mts and good luck!"
