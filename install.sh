7038fcefb77175bd708392ae276231753ca2f95b
ShadowFighter99 <50892896+ShadowFighter99@users.noreply.github.com>
2019-10-09 09:17:11 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "copying files to /usr/local/bin.."
-sudo chmod +x mts matisploit helpp
-sudo cp matisploit mts helpp /usr/local/bin
-echo "Instalation succesful , type mts and good luck!"
