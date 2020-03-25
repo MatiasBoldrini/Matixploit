f1aaa84d294036ed4108c3d80235dc61f61aee9d
Matías <50892896+ShadowFighter99@users.noreply.github.com>
2020-03-25 20:55:30 -0300

    Automatización de seleccion de interfaces
    Ahora las interfaces de red se pueden elegir con un numero

--- install.sh
+++ install.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
-source mtsconfig.sh
+  
+cd $HOME
 echo "###############################"
-echo -e " $blue ####INSTALANDO DEPENDENCIAS#### $nc"
+echo "####INSTALANDO DEPENDENCIAS####"
 echo "###############################"
 sudo apt -y install net-tools crunch nmap aircrack-ng macchanger cupp
-iwconfig
-echo -e " $red Nombre de Interfaz de Red : [wlo1/wlan0] $nc"
-read -p " mts ▷▷▷ "  interfaz 
-echo "export interfaz="$interfaz"" >> mtsconfig.sh
-echo -e " $red Nombre de Interfaz de Red en modo monitor? : [wlo1mon/wlan0mon] $nc"
-read -p " mts ▷▷▷ "  monitor
-echo "export monitor="$monitor"" >> mtsconfig.sh
+echo "Selecciona tu interfaz de red"
+cd /sys/class/net && select foo in *; do echo $foo selected; break; done
+ 
+mon="$foo"
+interfaz="$mon"
+mon+="mon"
+monitor="$mon"
 
 echo "Puedes ajustar todo esto tipeando mts 9"
-sleep 3
 echo "Puedes Borrar la carpeta Matixploit de tu carpeta personal"
+
+cd /home/matias/Programas/Matixploit/
+
+echo "interfaz="$interfaz"" >> mtsconfig.sh
+echo "$interfaz"
+echo "monitor="$monitor"" >> mtsconfig.sh
+echo "$monitor"
+
 sleep 3
-sudo rm README.md 
-sudo chmod +x mts mtsconfig.sh matisploit
+cd /home/matias/Programas/Matixploit/
+sudo chmod +777 mts mtsconfig.sh matisploit
 sudo cp mts mtsconfig.sh matisploit  /usr/local/bin/
+
 mts
